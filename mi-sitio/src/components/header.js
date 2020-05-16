493df824e0896d2d71b3e9af6cec6b5826addb34
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-16 01:09:11 -0500

    project update

---
+++ mi-sitio/src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => (
+  <header className="bg-gray-300">
+    <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
+      <div className="flex-1">
+        <h1>¡Hola! Soy Daniel Quintero</h1>
+        <p>Desarrollador de software e Ingeniero Industrial</p>
+      </div>
+      <img></img>
+    </div>
+  </header>
+)
