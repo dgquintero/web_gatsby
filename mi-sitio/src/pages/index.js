157d3a7c17a6d840942343acc82be130b56ca5af
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-19 00:26:06 -0500

    Project update

--- mi-sitio/src/pages/index.js
+++ mi-sitio/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Header from "../components/header";
 import Repos from "../components/repos";
+import Medium from "../components/medium"
 
 export default () => <div>
     <Header />
     <Repos />
+    <Medium />
 </div>
